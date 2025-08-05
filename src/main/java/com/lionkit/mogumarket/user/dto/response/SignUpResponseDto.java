22baa4184b26c197931c837cda8255d36d3a00b9
jungjiyu <libraryofjiyu@gmail.com>
2025-08-05 15:08:36 +0900

    chore: rename base package to com.lionkit.mogumarket

---
+++ src/main/java/com/lionkit/mogumarket/user/dto/response/SignUpResponseDto.java
@@ -0,0 +1,19 @@
+package com.lionkit.mogumarket.user.dto.response;
+
+import com.lionkit.mogumarket.user.entity.User;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class SignUpResponseDto {
+    private Long id;
+    private String username;
+
+    public static SignUpResponseDto fromEntity(User user) {
+        return SignUpResponseDto.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .build();
+    }
+}
