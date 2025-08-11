185bd12296af48c52ca517e95b789bb4d902cca6
SEUNGHYEOKNOH <109861378+SEUNGHYEOKNOH@users.noreply.github.com>
2025-08-11 15:39:13 +0900

    Refactor(product,search) -  product modifiyAt 기준 업데이트

--- src/main/java/com/lionkit/mogumarket/product/repository/ProductRepository.java
+++ src/main/java/com/lionkit/mogumarket/product/repository/ProductRepository.java
@@ -3,5 +3,9 @@ package com.lionkit.mogumarket.product.repository;
 import com.lionkit.mogumarket.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product>findByModifiedAtAfter(LocalDateTime lastSyncTime);
 }
