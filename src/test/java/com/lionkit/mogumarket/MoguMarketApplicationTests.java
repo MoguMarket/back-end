57d46c3f59070fe8029b5345b53c08604f74a2d0
jungjiyu <libraryofjiyu@gmail.com>
2025-08-15 10:29:18 +0900

    refactor : Rename main application class to MoguMarketApplication

--- src/test/java/com/lionkit/mogumarket/MoguMarketApplicationTests.java
+++ src/test/java/com/lionkit/mogumarket/MoguMarketApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OAuthTestApplicationTests {
+class MoguMarketApplicationTests {
 
 	@Test
 	void contextLoads() {
