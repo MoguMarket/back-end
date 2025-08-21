4eae919e2fbcf5444be7f78b0eabcb1e87e1d045
jungjiyu <142137932+jungjiyu@users.noreply.github.com>
2025-08-21 22:03:17 +0900

    chore: remove unused ElasticConfig

--- src/main/java/com/lionkit/mogumarket/search/config/ElasticsearchConfig.java
+++ src/main/java/com/lionkit/mogumarket/search/config/ElasticsearchConfig.java
@@ -1,35 +1,35 @@
-package com.lionkit.mogumarket.search.config;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-
-@Configuration
-@EnableElasticsearchRepositories
-public class ElasticsearchConfig extends ElasticsearchConfiguration  {
-
-    @Value("${spring.elasticsearch.username}")
-    private String username;
-
-    @Value("${spring.elasticsearch.password}")
-    private String password;
-
-    @Value("${spring.elasticsearch.uris}")
-    private String[] esHost;
-
-
-
-
-
-    @Override
-    public ClientConfiguration clientConfiguration() {
-        return ClientConfiguration.builder()
-                .connectedTo(esHost)
-                .withBasicAuth(username, password)
-                .build();
-
-    }
-
-}
+//package com.lionkit.mogumarket.search.config;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.elasticsearch.client.ClientConfiguration;
+//import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+//import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+//
+//@Configuration
+//@EnableElasticsearchRepositories(basePackages = "com.lionkit.mogumarket.search.repository")
+//public class ElasticsearchConfig extends ElasticsearchConfiguration  {
+//
+//    @Value("${spring.elasticsearch.username}")
+//    private String username;
+//
+//    @Value("${spring.elasticsearch.password}")
+//    private String password;
+//
+//    @Value("${spring.elasticsearch.uris}")
+//    private String[] esHost;
+//
+//
+//
+//
+//
+//    @Override
+//    public ClientConfiguration clientConfiguration() {
+//        return ClientConfiguration.builder()
+//                .connectedTo(esHost)
+//                .withBasicAuth(username, password)
+//                .build();
+//
+//    }
+//
+//}
