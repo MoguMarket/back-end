a5c95665af041644e889a1ecfa2106398ec1e967
jungjiyu <142137932+jungjiyu@users.noreply.github.com>
2025-08-23 14:43:34 +0900

    feat(config): update CORS settings to allow OPTIONS and production domain

--- src/main/java/com/lionkit/mogumarket/global/config/WebConfig.java
+++ src/main/java/com/lionkit/mogumarket/global/config/WebConfig.java
@@ -9,9 +9,18 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://localhost:3000", "http://localhost:5173")
-			.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-			.allowedHeaders("*")
-			.allowCredentials(true); // 쿠키/인증 포함 시 필요
+		registry.addMapping("/**")
+				.allowedOrigins(
+						"http://localhost:3000",
+						"http://localhost:5173",
+						"http://www.jungjiyu.com",
+						"https://localhost:3000",
+						"https://localhost:5173",
+						"https://www.jungjiyu.com"
+				)
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // OPTIONS 추가
+				.allowedHeaders("*")
+				.allowCredentials(true); // 쿠키/인증 포함 시 필요
 	}
+
 }
